7989ea9ef95898212db1f00e40f058689d611fa8
G-Audige <gloriaaudige@gmail.com>
2023-11-07 13:52:28 -0500

    Movement and collision detection added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('title').textContent = "Top Min"
 
 // Declarations
+let activeMin
 const blockSize = 25
 const border = 1
 const canvas = document.querySelector('#board')
@@ -39,80 +40,196 @@ const map = [
 // Classes and objects
 class Minikin {
     constructor(map, minX, minY, minColor) {
-        this.map = map
-        this.map[minY][minX] = 4
-        // this.current = false
-        this.currentMin = {
+        this.map = map      
+        this.position = {
             x: minX,
             y: minY,
-            color: minColor,
+            color: minColor
         }
+        this.map[minY][minX] = 4
+        this.active = false
+        document.addEventListener('keydown', this.moveMin)
     }
     switchState() {
-        if(this.current) {
-            this.current = false
+        if(this.active) {
+            this.active = false
         }
         else {
-            this.current = true
+            this.active = true
         }
-        return this.current
+    }
+    moveMin = ({keyCode}) => {
+        console.log("The keyCode is", keyCode)
+        let footprints = this.map[this.position.y][this.position.x]
+        let upDir = this.map[this.position.y - 1][this.position.x]
+        let downDir = this.map[this.position.y + 1][this.position.x]
+        let rightDir = this.map[this.position.y][this.position.x + 1]
+        let leftDir = this.map[this.position.y][this.position.x - 1]
+        // Up
+        if(keyCode === 87 || keyCode === 38) {
+            switch(upDir) {
+                case 2:
+                    footprints = 3
+                    upDir = 4
+                    this.position.y--
+                    this.drawMap(this.position.color) 
+                break;
+                case 3:
+                    footprints = 2
+                    upDir = 4
+                    this.position.y--
+                    this.drawMap(this.position.color) 
+                break;
+            }
+            this.drawMap(this.position.color) 
+        }
+        // Right
+        else if(keyCode === 68 || keyCode === 39) {
+            switch(rightDir) {
+                case 2:
+                    footprints = 3
+                    rightDir = 4
+                    this.position.x++
+                    this.drawMap(this.position.color) 
+                    break;
+                case 3:
+                    footprints = 2
+                    rightDir = 4
+                    this.position.x++
+                    this.drawMap(this.position.color) 
+                    break;
+            }
+            this.drawMap(this.position.color) 
+        }
+        // Down
+        else if(keyCode === 83 || keyCode === 40) {
+            switch(downDir) {
+                case 2:
+                    footprints = 3
+                    downDir = 4
+                    this.position.y++
+                    this.drawMap(this.position.color) 
+                    break;
+                case 3:
+                    footprints = 2
+                    downDir = 4
+                    this.position.y++
+                    this.drawMap(this.position.color) 
+                    break;
+            }
+            this.drawMap(this.position.color) 
+        }
+        // Left
+        else if(keyCode === 65 || keyCode === 37) {
+            switch(leftDir) {
+                case 2:
+                    footprints = 3
+                    leftDir = 4
+                    this.position.x--
+                    this.drawMap(this.position.color) 
+                    break;
+                case 3:
+                    footprints = 2
+                    leftDir = 4
+                    this.position.x--
+                    this.drawMap(this.position.color) 
+                    break;
+            }
+            this.drawMap(this.position.color) 
+        }
+    }
+    drawMap(minColor) {
+        for(let x = 0; x < this.map.length; x++) {
+            for(let y = 0; y < this.map[x].length; y++) {
+                const blockType = this.map[x][y]
+                let color = 'transparent'
+                switch(blockType) {
+                    case 0:
+                        color = 'pink'
+                    break;
+                    case 1:
+                        color = 'brown'
+                    break;
+                    case 2:
+                        color = 'yellow'
+                    break;
+                    case 3:
+                        color = 'orange'
+                    break;
+                    case 4:
+                        color = minColor
+                    break;
+                    case 5:
+                        color = 'red'
+                    break;
+                }
+                tbCtx.fillStyle = color
+                tbCtx.fillRect(y * (blockSize + border), x * (blockSize + border), blockSize, blockSize)
+            }
+        }
     }
 }
 
 // Functions
-function drawMap(minColor) {
-    for(let x = 0; x < map.length; x++) {
-        for(let y = 0; y < map[x].length; y++) {
-            const blockType = map[x][y]
-            let color = 'transparent'
-            switch(blockType) {
-                case 0:
-                    color = 'pink'
-                break;
-                case 1:
-                    color = 'brown'
-                break;
-                case 2:
-                    color = 'yellow'
-                break;
-                case 3:
-                    color = 'orange'
-                break;
-                case 4:
-                    color = minColor
-                break;
-                case 5:
-                    color = 'red'
-                break;
-            }
-            tbCtx.fillStyle = color
-            tbCtx.fillRect(y * (blockSize + border), x * (blockSize + border), blockSize, blockSize)
-        }
-    }
-}
-
-// function moveMin(e) {
-//     // Up
-//     if(e.keyCode === 87 || e.keyCode == 38) {
-//         if(map[minikin1.currentMin.y - 1][minikin1.currentMin.x] > 1) {
-//             if(map[minikin1.currentMin.y - 1][minikin1.currentMin.x] = 2)
-//             map[minikin1.currentMin.y][minikin1.currentMin.x] = 2
-//             map[minikin1.currentMin.y - 1][minikin1.currentMin.x] = 4
-//             minikin1.currentMin.y--
-//             drawMap(minikin1.currentMin.color)
+// function drawMap(minColor) {
+//     for(let x = 0; x < map.length; x++) {
+//         for(let y = 0; y < map[x].length; y++) {
+//             const blockType = map[x][y]
+//             let color = 'transparent'
+//             switch(blockType) {
+//                 case 0:
+//                     color = 'pink'
+//                 break;
+//                 case 1:
+//                     color = 'brown'
+//                 break;
+//                 case 2:
+//                     color = 'yellow'
+//                 break;
+//                 case 3:
+//                     color = 'orange'
+//                 break;
+//                 case 4:
+//                     color = minColor
+//                 break;
+//                 case 5:
+//                     color = 'red'
+//                 break;
+//             }
+//             tbCtx.fillStyle = color
+//             tbCtx.fillRect(y * (blockSize + border), x * (blockSize + border), blockSize, blockSize)
 //         }
-//         console.log(keyCode)
+//     }
+// }
+
+
+// Event Listeners
+// document.addEventListener('keydown', function(e) {
+//     const placement = map[activeMin.y][activeMin.x]
+//     const upDir = map[activeMin.y - 1][activeMin.x]
+//     const downDir = map[activeMin.y + 1][activeMin.x]
+//     const rightDir = map[activeMin.y][activeMin.x + 1]
+//     const leftDir = map[activeMin.y][activeMin.x - 1]
+//     // Up
+//     if(e.key === 'w' || e.key == 'ArrowUp') {
+//         console.log(e.key)
+//         switch(upDir) {
+//             case 0: {
+//                 placement = 0
+//                 upDir = 4
+//                 minikin1.currentMin.y--
+//                 drawMap(minikin1.currentMin.color)
+//             }  
+//             break;
+//         }
 //     }
 //     // Right
 //     // Down
 //     // Left
-// }
-
-
-
-// Event Listeners
-document.addEventListener('keydown', moveMin())
+// })
 
 // Page startup
-const minikin1 = new Minikin(map, 4, 5, 'blue')
-drawMap(minikin1.currentMin.color)
+const minikin1 = new Minikin(map, 2, 8, 'blue')
+// const minikin2 = new Minikin(map, 20, 8, 'purple')
+activeMin = minikin1
+activeMin.drawMap(activeMin.position.color)
