d7a1e3c67a51e17bdfe7ac4907f320e333faba3f
G-Audige <gloriaaudige@gmail.com>
2023-11-07 10:14:49 -0500

    Place character on board

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const map = [
     [0,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,0],
     [0,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,0],
     [0,0,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,0,0],
-    [0,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,0],
+    [1,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,1],
     [0,0,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,0,0],
     [0,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,0],
     [0,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,0],
@@ -37,7 +37,27 @@ const map = [
 ] 
 
 // Classes and objects
-
+class Minikin {
+    constructor(map, minX, minY, minColor) {
+        this.map = map
+        this.map[minY][minX] = 4
+        // this.current = false
+        this.currentMin = {
+            x: minX,
+            y: minY,
+            color: minColor,
+        }
+    }
+    switchState() {
+        if(this.current) {
+            this.current = false
+        }
+        else {
+            this.current = true
+        }
+        return this.current
+    }
+}
 
 // Functions
 function drawMap(minColor) {
@@ -71,9 +91,28 @@ function drawMap(minColor) {
     }
 }
 
+// function moveMin(e) {
+//     // Up
+//     if(e.keyCode === 87 || e.keyCode == 38) {
+//         if(map[minikin1.currentMin.y - 1][minikin1.currentMin.x] > 1) {
+//             if(map[minikin1.currentMin.y - 1][minikin1.currentMin.x] = 2)
+//             map[minikin1.currentMin.y][minikin1.currentMin.x] = 2
+//             map[minikin1.currentMin.y - 1][minikin1.currentMin.x] = 4
+//             minikin1.currentMin.y--
+//             drawMap(minikin1.currentMin.color)
+//         }
+//         console.log(keyCode)
+//     }
+//     // Right
+//     // Down
+//     // Left
+// }
+
+
 
 // Event Listeners
-
+document.addEventListener('keydown', moveMin())
 
 // Page startup
-drawMap()
+const minikin1 = new Minikin(map, 4, 5, 'blue')
+drawMap(minikin1.currentMin.color)
